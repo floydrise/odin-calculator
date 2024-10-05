1f700c7ec73d29e473847455c8a6ffd7b52ebd1d
Stefan <floydrise@gmail.com>
2024-10-05 22:03:08 +0300

    Finalise html and css, design done, start work on JS

---
+++ main.ts
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const clearBtn = document.querySelector(".clear");
+    const equal = document.querySelector(".equal");
+    const decimal = document.querySelector(".decimal");
+
+    const number = document.querySelectorAll(".number");
+    const operator = document.querySelectorAll(".operator");
+
+    const previousScreen = document.querySelector(".previous");
+    const currentScrenn = document.querySelector(".current");
+})
